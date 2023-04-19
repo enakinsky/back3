ed4d436e5e6bed6c31becd63a0e1f1991374921e
Erik <113479062+enakinsky@users.noreply.github.com>
2023-04-19 11:50:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -92,12 +92,13 @@ if (empty($_POST['super'])){
     $stmt = $conn->prepare("INSERT INTO FORMS(name, email, year, gender, limbs, biography) VALUES (:name, :email, :year, :gender, :limbs, :biography)");
     $rez=$stmt->execute(['name'=>"$name",'email'=>"$email", 'year'=>"$year", 'gender'=>"$gender", 'limbs'=>"$kol", 'biography'=>"$biography"]);
     $id_form=$conn->lastInsertId();
-    foreach ($_POST('superpower' as $value){
-       $stmt2=$conn->prepare("SELECT id_super FROM SUPERPOWERS WHERE superpower = $value");
-       $rez2=$stmt2->execute();}
-    $id_super=$conn->lastInsertId();
-    $stmt3=$conn->prepare("INSERT INTO FORM_SUPERPOWER(id_DATA_FORM, id_DATA_superpower) VALUES (:id_DATA_FORM, :id_DATA_superpower)");
-    $rez3=$stmt3->execute(['id_DATA_FORM'=>"$id_form", 'id_DATA_superpower'=>"$id_super"])
+       $stmt = $db->prepare("SELECT id FROM abilities WHERE ability_name = ?");
+    foreach ($abilities as $ability) {
+        $stmt->execute([$ability]);
+        $ability_id = $stmt->fetchColumn();
+ 
+        $stmt2 = $db->prepare("INSERT INTO FORN_SUPERPOWER (user_id, ability_id) VALUES (?, ?)");
+        $stmt2->execute([$id_form, $ability_id]);
   }
   include('form.php');
 }
